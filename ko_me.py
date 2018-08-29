a37bb6ea8772d18a5b0a2d294439cbfe85fb1e46
djinnome <djinnome@gmail.com>
2018-08-28 17:30:37 -0700

    Using genes from iJ1679

--- ko_me.py
+++ ko_me.py
@@ -39,7 +39,14 @@ def show_escher_map(me, solution=None):
     return view
 
 
-
+def get_genes( me ):
+    loci = set()
+    for reaction in me.reactions:
+        if isinstance(reaction, cobrame.core.reaction.TranscriptionReaction):
+            for rna_id in reaction.transcription_data.RNA_products:
+                locus_id = rna_id.replace("RNA_", "", 1)
+                loci.add( locus_id )
+    return loci
 
 # In[ ]:
 if __name__ == '__main__':
@@ -47,19 +54,17 @@ if __name__ == '__main__':
     parser.add_argument('kofile', type=argparse.FileType('r'))
     parser.add_argument('outdir')
     args = parser.parse_args()
-    for rxnId in open(args.kofile):
+    for gene_id in open(args.kofile):
         with open('/home/meuser/me_models/iJL1678b.pickle', 'rb') as f:
             me = pickle.load(f)
-            rxn = me.reactions.get_by_id( rxnId )
-            rxn.lower_bound = 0
-            rxn.upper_bound = 0
+            me.remove_genes_from_model( [gene_id] )
             solve_me_model(me, 1., min_mu = .1, precision=1e-2, using_soplex=False)
-            print("Biomass dilution for {} KO: {} ".format(rxnId,  me.solution.x_dict['biomass_dilution']))
+            print("Biomass dilution for {} KO: {} ".format(gene_id,  me.solution.x_dict['biomass_dilution']))
             metabolic_fluxes = pd.DataFrame({'MetabolicFlux': me.get_metabolic_flux()})
             expression_fluxes = pd.DataFrame({'TranscriptionFlux': me.get_transcription_flux(),
                                         'TranslationFlux': me.get_translation_flux()})
-            metabolic_fluxes.to_csv(os.path.join(args.outdir, 'MetabolicFlux.{}.csv'.format(rxn.id)))
-            expression_fluxes.to_csv(os.path.join(args.outdir('ExpressionFlux.{}.csv'.format(rxn.id))))
+            metabolic_fluxes.to_csv(os.path.join(args.outdir, 'MetabolicFlux.{}.csv'.format(gene_id)))
+            expression_fluxes.to_csv(os.path.join(args.outdir('ExpressionFlux.{}.csv'.format(gene_id))))
             
 
 
